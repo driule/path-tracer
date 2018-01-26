fa57b6e2c73df275748673ba2777df82f74f2ab5
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-01-26 12:07:46 +0100

    removed redundant method

--- Scene.h
+++ Scene.h
@@ -50,7 +50,6 @@ namespace Tmpl8 {
 		};
 		std::vector<Model*> models;
 
-		vec4 sampleNEE(Ray* ray, int depth, bool isLastIntersectedPrimitiveSpecular = false);
 		vec4 sample(Ray* ray, bool isLastPrimitiveSpecular = false);
 		vec4 sampleSkydome(Ray* ray);
 		vec4 illuminate(Ray* ray);
